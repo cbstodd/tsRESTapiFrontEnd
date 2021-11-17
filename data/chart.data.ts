9907ceb938dca57fa4ef12bcc48a167c6582ffdb
Colin Stodd <cbstodd@nillim.com>
2021-11-17 15:27:06 -0500

    Added banner image

--- data/chart.data.ts
+++ data/chart.data.ts
@@ -1,5 +1,84 @@
 export class ChartData {
-    chartData: ChartData[] = {
+
+
+    let;
+    months = [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'october',
+        'november',
+        'december'
+    ];
+
+    const;
+    barColors = ['red', 'green', 'blue', 'orange', 'brown'];
+    let;
+    xyValues = [{
+        'january': {
+            'refinance': 2000000,
+            'purchase': 1800000
+        }
+    },
+        {
+            'february': {
+                'refinance': 3500000,
+                'purchase': 1920000
+            },
+        },
+        {
+            'march': {
+                'refinance': 1400000,
+                'purchase': 800000
+            }
+        },
+        {
+            'april': {
+                'refinance': 4100000,
+                'purchase': 2800000
+            }
+        },
+        {
+            'may': {
+                'refinance': 2000000,
+                'purchase': 1800000
+            }
+        },
+        {
+            'june': {
+                'refinance': 2400000,
+                'purchase': 1900000
+            }
+        }];
+
+    new;
+
+    Chart(
+
+    'salesChart';
+, {
+    type: 'bar';
+,
+    data: {
+        labels: months[],
+        datasets: [{
+            backgroundColor: barColors,
+            data: xyValues
+        }]
+    };
+,
+    options: { ... };
+}
+
+)
+;
+
+ChartData[] = {
     chartData: [
         {
             'january': {
@@ -39,6 +118,7 @@ export class ChartData {
         }
     ];
 };
-return chartData;
+return chartData
 
 
+}
